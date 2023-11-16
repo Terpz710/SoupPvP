b291120fefd223cde2936282761c83b57b1ef60d
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-11-16 15:17:09 -0800

    1.0.2-BETA

--- src/Terpz710/SoupPvP/Main.php
+++ src/Terpz710/SoupPvP/Main.php
@@ -7,38 +7,51 @@ use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerItemConsumeEvent;
 use pocketmine\item\MushroomStew;
 use pocketmine\plugin\PluginBase;
-use pocketmine\Player;
+use pocketmine\player\Player;
 
 class Main extends PluginBase implements Listener {
 
+    /** @var array */
+    private $allowedWorlds = [];
+
     public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+        $this->saveDefaultConfig();
+        $this->allowedWorlds = $this->getConfig()->get("allowedWorlds", []);
     }
 
     public function onPlayerInteract(PlayerInteractEvent $event) {
-    $player = $event->getPlayer();
-    $item = $event->getItem();
+        $player = $event->getPlayer();
+        $item = $event->getItem();
 
-    if ($event->getAction() === PlayerInteractEvent::RIGHT_CLICK_BLOCK && $item instanceof MushroomStew) {
-        $event->cancel();
+        if ($event->getAction() === PlayerInteractEvent::RIGHT_CLICK_BLOCK && $item instanceof MushroomStew) {
+            $event->cancel();
 
-        if ($player->getHealth() < $player->getMaxHealth()) {
-            $healAmount = min(4, $player->getMaxHealth() - $player->getHealth());
-            $player->setHealth($player->getHealth() + $healAmount);
-            $player->sendMessage("Healed§b $healAmount §rtotal hearts!");
-            $player->getInventory()->removeItem($item);
-        } else {
-            $player->sendMessage("You have §bfull health§r already!");
+            if ($this->isPlayerInAllowedWorld($player)) {
+                if ($player->getHealth() < $player->getMaxHealth()) {
+                    $healAmount = min(4, $player->getMaxHealth() - $player->getHealth());
+                    $player->setHealth($player->getHealth() + $healAmount);
+                    $player->sendMessage("Healed §b$healAmount §rtotal hearts!");
+                    $player->getInventory()->removeItem($item);
+                } else {
+                    $player->sendMessage("You have §bfull health§r already!");
+                }
+            }
         }
     }
-}
 
     public function onPlayerItemConsume(PlayerItemConsumeEvent $event) {
         $player = $event->getPlayer();
         $item = $event->getItem();
 
-        if ($item instanceof MushroomStew) {
+        if ($item instanceof MushroomStew && !$this->isPlayerInAllowedWorld($player)) 
             $event->cancel();
         }
     }
+
+    private function isPlayerInAllowedWorld(Player $player): bool {
+        return in_array($player->getWorld()->getName(), $this->allowedWorlds, true);
+    }
 }
+
